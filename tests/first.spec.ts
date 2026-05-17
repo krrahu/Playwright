40878a4c95b442db42af581712ef242477de4347
krrahu <rahul.baliyase91@gmail.com>
2026-05-17 16:15:04 +0530

    first

---
+++ tests/first.spec.ts
@@ -0,0 +1,13 @@
+ import {test,expect} from "@playwright/test";
+ //syntax
+ test("verfy the title",()=>{
+    console.log('sample test')
+ }
+ )
+
+ // to run this --> npx playwright test 
+
+ // not need to define browser name like webdriver driver = new Chromedriver(); automatically it take browser deatils fron config file and it will launch 
+ // is faster than selenium
+ // 
+
